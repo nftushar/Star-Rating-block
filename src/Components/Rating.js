5cc9792dd4a58310fb0a5fd9f89bad7a5aa8fb11
nftushar <nftushar5200@gmail.com>
2023-09-12 11:42:00 +0600

    Another Static Design update

--- src/Components/Rating.js
+++ src/Components/Rating.js
@@ -1,20 +1,23 @@
-import { getArrFromNum } from '../utils/functions';
-import { solidStar, outlineStar } from '../utils/icons';
+import { getArrFromNum } from "../utils/functions";
+import { solidStar, outlineStar } from "../utils/icons";
 
 const Rating = ({ attributes }) => {
-    const { ratingScale, iconStyle, prefix } = attributes;
+  const { ratingScale, iconStyle, prefix } = attributes;
 
-    return <div className="bBlocksProductReview">
-        <span className="ratingPrefix">{prefix}</span>
-
-        <div className="stars">
-            {getArrFromNum(ratingScale).map((index) => {
-                return <span key={index} className="star">
-                    {'solid' === iconStyle ? solidStar : outlineStar}
-                    <span className="starFill">{solidStar}</span>
-                </span>
-            })}
-        </div>
+  return (
+    <div className="bBlocksProductReview">
+      <div className="stars">
+        {getArrFromNum(ratingScale).map((index) => {
+          return (
+            <span key={index} className="star">
+              {"solid" === iconStyle ? solidStar : outlineStar}
+              <span className="starFill">{solidStar}</span>
+            </span>
+          );
+        })}
+      </div>
+      <span className="ratingPrefix">{prefix}</span>
     </div>
-}
+  );
+};
 export default Rating;
