076cacc42170f2ca8beca1f4dd69abca93a8c8cb
nftushar <nftushar5200@gmail.com>
2023-12-02 19:32:47 +0600

    dynamic text color added

--- src/Path.js
+++ src/Path.js
@@ -5,10 +5,10 @@ const Path = ({ attributes }) => {
     const { svgPath, iconStyle, prefix, strokeColor, cId } = attributes;
 
     return (
-        <div id={`bBlocksPath-${cId}`} className="bBlocksPath" >
+        <div id={`bBlocksPath-${cId}`} className="bBlocksPath">
             <svg width="300" height="300" xmlns="http://www.w3.org/2000/svg">
                 <path id="MyPath" fill="none" stroke={strokeColor} d={svgPath} />
-                <text fontSize="24px" fill="#333" textAnchor="middle" dy="-10">
+                <text className='text' fontSize="24px" fill="#333" textAnchor="middle" dy="-10">
                     <textPath href="#MyPath" startOffset="5%">
                         Quick brown fox jumps over the lazy dog. Quick brown fox jumps over the lazy dog. Quick brown fox jumps over the lazy dog.
                     </textPath>
--- src/Style.js
+++ src/Style.js
@@ -5,59 +5,26 @@ const Style = ({ attributes, clientId }) => {
     const { path, gap, alignment, textTypo, textColor, textShadow } = attributes;
 
     const pathSl = `#bBlocksPath-${clientId}`;
-    // div#bBlocksPath-b7cd7963-594f-425b-ab8b-9f70e3c560eb
-    const textSl = `${pathSl} .paths .text`;
-    const intAndDec = (path + '').split('.');
-    const pathInt = parseInt(intAndDec[0]);
-    const pathDec = parseInt(intAndDec[1] || 0);
+    const textSl = `${pathSl} textPath`;
 
     return <style dangerouslySetInnerHTML={{
         __html: `
         ${getTypoCSS(``, textTypo)?.googleFontLink}
         ${getTypoCSS(`${pathSl} .pathPrefix`, textTypo)?.styles}
 
-          ${pathSl} {
-            
+        ${pathSl} {
             justify-content: ${alignment};
             gap: ${gap};
-            color: ${textColor}; 
-          }
-
-          ${pathSl} .pathPrefix{
-            text-shadow: ${getMultiShadowCSS(textShadow, 'text')}
-          }
-
-        ${getArrFromNum(pathInt).map((index) => `${textSl}:nth-child(${index}) .textFill`).join(', ')}{
-            width: 100%;
+            fill: ${textColor}; 
         }
 
-        ${textSl}:nth-child(${pathInt + 1}) .textFill{
-            width: ${(() => {
-                switch (pathDec) {
-                    case 1:
-                        return 25;
-                    case 2:
-                        return 30;
-                    case 3:
-                        return 35;
-                    case 4:
-                        return 42.5;
-                    case 5:
-                        return 50;
-                    case 6:
-                        return 57.5;
-                    case 7:
-                        return 65;
-                    case 8:
-                        return 70;
-                    case 9:
-                        return 75;
-                    default:
-                        return 0
-                }
-            })()}%;
+        ${textSl} {
+            text-shadow: ${getMultiShadowCSS(textShadow, 'text')};
+            fill: ${textColor}; 
+        } 
         }
     `}}
     />
 }
+
 export default Style;
